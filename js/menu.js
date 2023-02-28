b5341f524586e88b60d28c0c7ec04e020bf972ed
Federico Centurion <info.federicocen@gmail.com>
2023-02-28 18:51:23 -0300

    borro código innecesario

--- js/menu.js
+++ js/menu.js
@@ -12,29 +12,3 @@ window.addEventListener('click', e=>{
     }
 })
 
-/*
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("form").addEventListener('submit', validarFormulario); 
-  });
-  
-  function validarFormulario(evento) {
-    evento.preventDefault();
-    var nombre = document.getElementById('nombre').value;
-    if( nombre == null || nombre.length == 0 || /^\s+$/.test(nombre) ) {
-       var nom = document.querySelector('.alerta');
-       nom.classList.add('visible');
-      } else {
-        nom.classList.remove('visible');
-      }
-    var correo = document.getElementById('email').value;
-    if(correo == null) {
-      alert('No has escrito nada en el usuario');
-      return;
-    }
-    var mensaje = document.getElementById('mensaje').value;
-    if (mensaje == 0) {
-      alert('No has colocado el mensaje');
-      return;
-    }
-    this.submit();
-  }*/
